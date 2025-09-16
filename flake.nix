b3883d5863acba9aa9f9651383f68e8d4adcd25e
ajay-nagyal <anagyal@insilica.co>
2025-09-16 10:37:15 -0400

    init(flake): init a nix flake and add r packages + conda

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "FDA BioBrick";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system:
+      with import nixpkgs { inherit system; }; {
+        devShells.default = mkShell {
+          buildInputs = [
+            R
+            rPackages.rvest
+            rPackages.here
+            rPackages.purrr
+            rPackages.stringr
+            rPackages.dplyr
+            rPackages.tidyr
+            rPackages.repurrrsive
+            rPackages.rio
+            rPackages.arrow
+            rPackages.tidyverse
+            rPackages.reticulate
+            rPackages.data_table
+            rPackages.htmlTable
+            rPackages.remotes
+            rPackages.chromote
+            pkgs.python313Packages.conda
+            pkgs.dvc
+          ];
+        };
+      });
+}
