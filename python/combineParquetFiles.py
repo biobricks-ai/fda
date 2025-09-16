d9b5081b1f35748b3eaf67545903067a0c258ca7
ajay-nagyal <anagyal@insilica.co>
2025-09-16 15:23:30 -0400

    fix(python): remove shebangs from python scripts

--- python/combineParquetFiles.py
+++ python/combineParquetFiles.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Fri Apr 22 15:48:23 2022
 
@@ -14,22 +12,34 @@ import argparse
 import glob
 
 
-parser = argparse.ArgumentParser(description='Combines a set of parquet files into a single file')
-parser.add_argument('-fileInputDir','--inputdir', help='Directory starting from fda/ where parquet data to join is located', required=True)
-parser.add_argument('-fileOutput','--output', help='Output path for combined parquet file', required=True)
+parser = argparse.ArgumentParser(
+    description="Combines a set of parquet files into a single file"
+)
+parser.add_argument(
+    "-fileInputDir",
+    "--inputdir",
+    help="Directory starting from fda/ where parquet data to join is located",
+    required=True,
+)
+parser.add_argument(
+    "-fileOutput",
+    "--output",
+    help="Output path for combined parquet file",
+    required=True,
+)
 args = vars(parser.parse_args())
 
 
 def combineDrugParquetFiles(inputDir, outputFile):
-    AllTableNames=[]
-    inputFileList=glob.glob(str(str(inputDir)+'*parquet'))
+    AllTableNames = []
+    inputFileList = glob.glob(str(str(inputDir) + "*parquet"))
     for i in inputFileList:
-        i_temp=pq.read_table(i).to_pandas()
+        i_temp = pq.read_table(i).to_pandas()
         AllTableNames.append(i_temp)
     frame = pd.concat(AllTableNames, axis=0, ignore_index=True)
     table = pa.Table.from_pandas(frame)
     pq.write_table(table, str(outputFile))
-   
-if __name__ == "__main__":
-    combineDrugParquetFiles(args['inputdir'],args['output'])
 
+
+if __name__ == "__main__":
+    combineDrugParquetFiles(args["inputdir"], args["output"])
--- python/jsonToParquet.py
+++ python/jsonToParquet.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Thu Apr 21 09:51:23 2022
 
@@ -12,18 +10,21 @@ import pyarrow.parquet as pq
 import pyarrow.dataset as ds
 import argparse
 
-parser = argparse.ArgumentParser(description='Converts json drug data file directly to parquet file')
-parser.add_argument('-fileInput','--input', help='Input json file', required=True)
-parser.add_argument('-fileOutput','--output', help='Output parquet file', required=True)
+parser = argparse.ArgumentParser(
+    description="Converts json drug data file directly to parquet file"
+)
+parser.add_argument("-fileInput", "--input", help="Input json file", required=True)
+parser.add_argument(
+    "-fileOutput", "--output", help="Output parquet file", required=True
+)
 args = vars(parser.parse_args())
 
 
-def readDrugLabelJson(inputJsonFile,outputParquetFile):
+def readDrugLabelJson(inputJsonFile, outputParquetFile):
     dataLabel = pd.read_json(inputJsonFile)
     table = pa.Table.from_pandas(dataLabel)
-    pq.write_table(table,outputParquetFile)
+    pq.write_table(table, outputParquetFile)
 
 
 if __name__ == "__main__":
-    readDrugLabelJson(args['input'],args['output'])
-
+    readDrugLabelJson(args["input"], args["output"])
